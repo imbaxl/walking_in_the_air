70c0f506fb6ca83e719a3415d2d377bb5af9da9a
imbaxl <60095551+imbaxl@users.noreply.github.com>
2025-06-16 03:41:27 +0800

    Quartz sync: Jun 16, 2025, 3:41 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,7 +41,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph([
+	localGraph:[
+depth:3,
+fontSize: 1,
+linkDistance:20,
+],
+]),
     // Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
